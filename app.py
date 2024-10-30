08be23209c683c4f6cb3d6e4a194d23ddde45504
FabricioFavero <fabriciofavero@hotmail.com>
2024-10-30 19:42:10 -0300

    variaveis criadas para api

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import requests
-import streamlit as st 
+import streamlit as st
 
+st.image("https://i.imgur.com/SmktDIH.png")
+
+st.title ("Letra de Músicas")
+
+banda = st.text_input ("Digite o nome da banda", key="banda")
+musica = st.text_input ("Digite o nome da Música", key="musica")
